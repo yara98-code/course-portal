8b83631bca31ee1e8108f6e3d35a0f57e051049f
yara98-code <eng.yara.mansour@gmail.com>
2026-07-05 11:01:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,7 @@ def subscribe():
     return jsonify({"success": True, "message": "Subscribed successfully!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port, debug=False)
