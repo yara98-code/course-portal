4a972b87c13d469e6b8be1ba1a14d491b7f64d56
yara98-code <eng.yara.mansour@gmail.com>
2026-07-05 10:25:43 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = "AWD@123"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/subscription_portal"
+    SQLALCHEMY_DATABASE_URI = "postgresql://subscription_db_k5sh_user:uKEZYivYYuIqtOxzrVlwcBEUptKiISLz@dpg-d9595s7avr4c73a3pj40-a.oregon-postgres.render.com/subscription_db_k5sh"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
