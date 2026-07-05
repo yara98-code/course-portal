dda837838d45de68c9cf08d1145f977bdd9ca1af
yara98-code <eng.yara.mansour@gmail.com>
2026-07-05 11:28:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -153,5 +153,7 @@ def subscribe():
 
 if __name__ == "__main__":
     import os
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
